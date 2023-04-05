9f5387e56dc3fbd35c3e892685bfe59b6a566991
Mohammad Rafly <mohammadrafly19@gmail.com>
2023-04-06 05:00:50 +0700

    add dashboard.js

---
+++ src/pages/dashboard.js
@@ -0,0 +1,66 @@
+import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+
+const DASHBOARD_API_URL = 'http://localhost:8080/api/my-profile/';
+
+export default function Dashboard() {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+  const [dashboardData, setDashboardData] = useState({});
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    const fetchDashboardData = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        if (!token) {
+          router.push('/');
+        }
+
+        const response = await fetch(DASHBOARD_API_URL + token, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        const data = await response.json();
+        setDashboardData(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('An error occurred:', error);
+        setErrorMessage('Unable to fetch dashboard data. Please try again.');
+      }
+    };
+
+    fetchDashboardData();
+  }, [router]);
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    router.push('/');
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Dashboard - Auth Test</title>
+        <meta name="description" content="Dashboard page" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <h1>Welcome to the Dashboard</h1>
+          <p>{dashboardData.message}</p>
+          {errorMessage && <p>{errorMessage}</p>}
+          <button onClick={handleLogout}>Logout</button>
+        </>
+      )}
+    </>
+  );
+}
