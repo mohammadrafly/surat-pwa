8fecef06b44b702f71a605a284052f28b2cfcf9f
Mohammad Rafly <mohammadrafly19@gmail.com>
2023-05-15 14:00:24 +0700

    test #2

--- src/pages/create/surat-kelahiran.js
+++ src/pages/create/surat-kelahiran.js
@@ -51,8 +51,6 @@ export default function Dashboard() {
         headers: {
           'Content-Type': 'application/json',
           'Authorization': `Bearer ${token}`,
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization' // Allow these headers
         },
         body: JSON.stringify(inputValues),
       });
